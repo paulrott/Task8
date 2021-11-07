29854131fbba150681627aa70b76213a63186d41
gromov.p <paulrott76@gmail.com>
2021-11-07 20:58:30 +0300

    Initial commit Task8

---
+++ Task8.py
@@ -0,0 +1,5 @@
+number_list = ['Name', 'Abstract', 'Cooking time: min', 60 ,'Full description', 'Number of cooking stages :',3, 'List of ingredients']
+print(number_list)
+
+extract_list = number_list[:3]
+print(extract_list)
